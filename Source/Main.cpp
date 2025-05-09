e8e769e5e83f6d1f7ddef666cab4735888b4f420
Vincent Berthiaume <vberthiaume@gmail.com>
2025-05-09 08:23:50 -0400

    cleanup

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -33,27 +33,31 @@ int main (int argc, char* argv[])
     auto                   mmFile = juce::File (kSharedMemoryMapFilepath);
     juce::MemoryMappedFile logDebugMapping (mmFile, juce::MemoryMappedFile::readOnly, false);
     DebugLog*              m_pLogDebug = (DebugLog*) logDebugMapping.getData();
-    if (m_pLogDebug)
+    if (! m_pLogDebug)
     {
-        //latch tail to head
-        size_t logTail (m_pLogDebug->logHead);
+        std::cerr << "\n\nError! Failed to map file " << kSharedMemoryMapFilepath << " to memory\n\n\n";
+        return 1;
+    }
 
-        while (true)
+    //latch tail to head
+    size_t logTail (m_pLogDebug->logHead);
+
+    while (true)
+    {
+        //if head moved forward
+        while (logTail != m_pLogDebug->logHead)
         {
-            //if head moved forward
-            while (logTail != m_pLogDebug->logHead)
-            {
-                auto& logDebugEntry = m_pLogDebug->log[logTail];
+            auto& logDebugEntry = m_pLogDebug->log[logTail];
 
-                printDebugLogEntry (logDebugEntry);
+            printDebugLogEntry (logDebugEntry);
 
-                //try to catch up with head
-                logTail = (logTail + 1) & (kMaxDebugEntries - 1);
+            //try to catch up with head
+            logTail = (logTail + 1) & (kMaxDebugEntries - 1);
 
-                //wait for 10 milliseconds...if there's no activity. (not too sure what that means)
-                juce::Time::waitForMillisecondCounter (juce::Time::getMillisecondCounter() + 10);
-            }
+            //wait for 10 milliseconds...if there's no activity. (not too sure what that means)
+            juce::Time::waitForMillisecondCounter (juce::Time::getMillisecondCounter() + 10);
         }
     }
+
     return 0;
 }
