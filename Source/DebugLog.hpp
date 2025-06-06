b6d6699d4b37e4b27afd20f9a5cf72319610256f
Vincent Berthiaume <vberthiaume@gmail.com>
2025-06-06 11:37:25 -0400

    cleanup

--- Source/DebugLog.hpp
+++ Source/DebugLog.hpp
@@ -2,16 +2,17 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+#define ENABLE_DEBUG_LOG 1
+
 #if JUCE_WINDOWS
 static const juce::File memoryMappedFile { "C:/temp/ProPhatSharedMemory" };
 #else
 static const juce::File memoryMappedFile { "/tmp/ProPhatSharedMemory" };
 #endif
 
-//static const auto memoryMappedFile { juce::File::getSpecialLocation(juce::File::SpecialLocationType::tempDirectory).getChildFile("ProPhatSharedMemory") };
-
 static constexpr auto kMaxDebugEntries { 2048 };
 
+//======================================================================================
 struct DebugLogEntry
 {
     juce::int64 timeSinceLastCall {};
@@ -19,12 +20,14 @@ struct DebugLogEntry
     int         curEffect {};
 };
 
+//======================================================================================
 struct DebugLog
 {
     DebugLogEntry   log[kMaxDebugEntries];
     volatile size_t logHead {};
 };
 
+//======================================================================================
 inline void printDebugLogEntry (DebugLogEntry& logEntry)
 {
     std::cout << "timeSinceLastCall: " << juce::String (logEntry.timeSinceLastCall)
