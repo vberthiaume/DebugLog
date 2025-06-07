0d18c51b0248d8f4b8c384bf2328bef46360fde0
Vincent Berthiaume <vberthiaume@gmail.com>
2025-06-07 10:24:13 -0400

    building

--- Source/DebugLog.hpp
+++ Source/DebugLog.hpp
@@ -3,6 +3,7 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 
 #define ENABLE_DEBUG_LOG 1
+#define ENABLE_GAIN_LOGGING ENABLE_DEBUG_LOG && 0
 
 #if JUCE_WINDOWS
 static const juce::File memoryMappedFile { "C:/temp/ProPhatSharedMemory" };
@@ -18,6 +19,10 @@ struct DebugLogEntry
     juce::int64 timeSinceLastCall {};
     juce::int64 processCallDuration {};
     int         curEffect {};
+#if ENABLE_GAIN_LOGGING
+    //this cannot be a string, it needs to be a fixed size char array, because the printing is based on size
+    char gainRamp[5120]; //it takes 7 chars per float, so approximation here is 10x the buffer size i have lol
+#endif
 };
 
 //======================================================================================
@@ -31,6 +36,10 @@ struct DebugLog
 inline void printDebugLogEntry (DebugLogEntry& logEntry)
 {
     std::cout << "time since: " << juce::String (logEntry.timeSinceLastCall)
-    << ", duration: " << juce::String (logEntry.processCallDuration)
-    << ", Effect: " << logEntry.curEffect << std::endl;
+              << ", duration: " << juce::String (logEntry.processCallDuration)
+              << ", effect: " << logEntry.curEffect
+#if ENABLE_GAIN_LOGGING
+              << ", gainRamp: " << logEntry.gainRamp
+#endif
+              << "\n";
 }
